2cd747be7117ea122829ebfc21cf80c597b0b9f6
gareth0712 <gareth_0712@hotmail.com>
2021-10-07 03:06:54 +0800

    Update call to action section

--- components/CallToAction.vue
+++ components/CallToAction.vue
@@ -1,31 +1,59 @@
 <template>
-  <section id="Contact" class="pt-120 pb-100 blue-bg">
-          <div class="container">
-              <div class="row align-items-center">
-                  <div class="col-md-8">
-                      <!-- cta title -->
-                      <div class="callto-heading">
-                          <span class="schoolbell">Free Delivery on All Orders</span>
-                          <h2 class="text-white">We are Ready to Take Your Call! 000 8888 999</h2>
-                      </div>
-                      <!-- cta title -->
-                  </div>
-                  <div class="col-md-4">
-                      <div class="call-action-btn text-right">
-                          <a href="#" class="btn">Book Your Order Now</a>
-                      </div>
-                  </div>
-              </div>
+  <section id="Contact" class="pt-120 pb-100">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-8">
+          <!-- cta title -->
+          <div class="callto-heading">
+            <span class="schoolbell">做好風險管理 讓財富穩健增長</span>
+            <h2 class="text-blue">
+              立即聯絡我們<br />讓我們為你訂制累積財富的計劃
+            </h2>
           </div>
-      </section>
+          <!-- cta title -->
+        </div>
+        <div class="col-md-4">
+          <div class="call-action-btn text-right">
+            <div @click="openBrowser(whatsappLink)" class="btn">聯絡我們</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-    export default {
-        name: "CallToAction"
+export default {
+  name: 'CallToAction',
+  data() {
+    return {
+      whatsappLink:
+        'https://api.whatsapp.com/send?phone=85254407482&app=facebook&entry_point=page_cta&fbclid=IwAR0EM0JoH7Iqs4fJk-Dz3NDpr1fyAUECCNICPy7xz3js9tLFeL8t-whWFPY'
+    };
+  },
+  methods: {
+    openBrowser(link) {
+      if (!link) return;
+      window.open(link);
     }
+  }
+};
 </script>
 
 <style scoped>
+#Contact {
+  background-color: #f0f3bd;
+}
 
+.schoolbell {
+  font-family: 'genJyuuGothic-medium';
+}
+
+.text-blue {
+  font-family: 'genJyuuGothic-medium';
+}
+
+.btn {
+  font-family: 'genJyuuGothic-regular';
+}
 </style>
